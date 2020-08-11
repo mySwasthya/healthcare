d3590f604307557b415b168e21e6665c26cb2429
Puneet112 <puneetsoodan112@gmail.com>
2020-08-12 02:46:22 +0530

    Added Carousel to the Main Component

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,25 +1,13 @@
 import React, {Component}  from 'react';
 import Header from './HeaderComponent';
-import Footer from './FooterComponent';
-import {Jumbotron} from 'reactstrap';
+import Carousel from './Carousel';
 
 class Main extends Component {
   render() {
     return(
       <div>
         <Header />
-        <Jumbotron >
-          <div className="container">
-            <div className="row row-header">
-                
-                <h1 className="">We are here to take care of you</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt uconsequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum..</p>
-
-            </div>
-          </div>
-        </Jumbotron>
-
-        <Footer />
+        <Carousel />
       </div>
     );
   }
