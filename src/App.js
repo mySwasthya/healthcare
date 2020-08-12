52dd8df76db0237e4a74a7b1488f0989aee040b6
Aditya Raina <adityaraina060199@gmail.com>
2020-08-12 23:46:44 +0530

    User Signup

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
 import Main from './components/MainComponent';
 import './App.css';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter,Route } from 'react-router-dom';
+import Signup from "./components/UsersignupComponent";
 
 class App extends Component {
    render() {
     return (
-      <BrowserRouter> 
-            <Main />
+      <BrowserRouter>
+      <div>
+      <Route>
+      <Route exact path="/" component={Main} />
+      <Route path="/home" component={Main} />
+      <Route path="/signup" component={Signup} />
+      </Route>
+      </div>
       </BrowserRouter>
     );
-   } 
-   
+   }
+
 }
 
 export default App;
