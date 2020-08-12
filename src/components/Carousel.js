5eafe180bddc174307b2498b54f5dbd81db74269
Puneet112 <puneetsoodan112@gmail.com>
2020-08-13 03:18:47 +0530

    Home Component

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Button } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 class Carousel extends Component {
     render() {
@@ -8,8 +9,12 @@ class Carousel extends Component {
                 <div id="landing-header">               
                     <h1>We are here to take care of you</h1>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt uconsequat.</p>
-                    <Button size="lg" className="button1">Get Started</Button>
-                    <Button size="lg" className="button2">Visit Pharmacy Store</Button>
+                    <Button size="lg" className="button1">
+                        <Link to="/home">Get Started</Link>
+                    </Button>
+                    <Button size="lg" className="button2">
+                        <Link to="">Visit Pharmacy Store</Link>
+                    </Button>
                 </div>
                 <ul class="slideshow">
                     <li></li>
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,6 +2,7 @@ import React, {Component}  from 'react';
 import { Switch, Route, Redirect } from "react-router-dom";
 import Header from './HeaderComponent';
 import Carousel from './Carousel';
+import Home from './Home';
 import Contact from './Contact';
 
 class Main extends Component {
@@ -11,6 +12,7 @@ class Main extends Component {
         <Header />
         <Switch>
             <Route exact path="/" component={Carousel} />
+            <Route exact path="/home" component={Home} />
             <Route exact path="/contact" component={Contact} />
             <Redirect to="/" />
         </Switch>
