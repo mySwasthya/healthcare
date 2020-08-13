480e0ea909820769bea5712506f4f150b97b0343
Ayush <ayupradhan1999@gmail.com>
2020-08-14 02:13:19 +0530

    contact page and footer

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,34 +1,41 @@
-import React, {Component} from 'react';
-import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  Card,
+  CardBody,
+  CardHeader,
+  Media,
+} from "reactstrap";
+import { Link } from "react-router-dom";
 
 function About() {
-  return(
-    <div >
-        <img src="/assets/images/HealthQuote.jpg"  className="health-quote"/>
-        <div className="container about-container">
+  return (
+    <div>
+      <img src="/assets/images/HealthQuote.jpg" className="health-quote" />
+      <div className="container about-container">
         <h2>About us</h2>
-        <hr  className="hr-style"/>
-        
-          <p>
-          Welcome to Swasthya, our mission here is to connect with the people of India and 
-          provide them with best medical information. Users can check for symptoms for the 
-          diseases and find the best suitable medicines and drugs. This platform will also 
-          help doctors to publish articles and blogs related to their research field.
-         <br/><br/>
-          So join with us on our mission to make India healthy.</p> 
+        <hr className="hr-style" />
 
-          
-          <p>Sincerely,<br/> Swasthya team</p>
-          <hr className="hr-style"/>       
+        <p>
+          Welcome to Swasthya, our mission here is to connect with the people of
+          India and provide them with best medical information. Users can check
+          for symptoms for the diseases and find the best suitable medicines and
+          drugs. This platform will also help doctors to publish articles and
+          blogs related to their research field.
+          <br />
+          <br />
+          So join with us on our mission to make India healthy.
+        </p>
+
+        <p>
+          Sincerely,
+          <br /> Swasthya team
+        </p>
+        <hr className="hr-style" />
       </div>
     </div>
-      
-    
   );
 }
-
-
-
 
 export default About;
--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -1,51 +1,80 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Footer(props) {
-    return(
-    <div className="footer">
-        <div className="container">
-            <div className="row justify-content-center">             
-                <div className="col-4 offset-1 col-sm-2">
-                    <h5>Links</h5>
-                    <ul className="list-unstyled">
-                        <li><Link to="/home">Home</Link></li>
-                        <li><Link to="/aboutus">About us</Link></li>
-                        <li><Link to="/menu">Menu</Link></li>
-                        <li><Link to="/contactus">Contact us</Link></li>
-                    </ul>
-                </div>
-                <div className="col-7 col-sm-5">
-                    <h5>Our Address</h5>
-                    <address>
-		                  .....<br />
-		                  .....<br />
-		                <br />
-		              <i className="fa fa-phone fa-lg"></i>: +852 1234 5678<br />
-		              
-		              <i className="fa fa-envelope fa-lg"></i>: <a href="mailto:confusion@food.net">
-                         example@example.net</a>
-                    </address>
-                </div>
-                <div className="col-12 col-sm-4 align-self-center">
-                    <div className="text-center">
-                        <a className="btn btn-social-icon btn-google" href="http://google.com/+"><i className="fa fa-google-plus"></i></a>
-                        <a className="btn btn-social-icon btn-facebook" href="http://www.facebook.com/profile.php?id="><i className="fa fa-facebook"></i></a>
-                        <a className="btn btn-social-icon btn-linkedin" href="http://www.linkedin.com/in/"><i className="fa fa-linkedin"></i></a>
-                        <a className="btn btn-social-icon btn-twitter" href="http://twitter.com/"><i className="fa fa-twitter"></i></a>
-                        <a className="btn btn-social-icon btn-google" href="http://youtube.com/"><i className="fa fa-youtube"></i></a>
-                        <a className="btn btn-social-icon" href="mailto:"><i className="fa fa-envelope-o"></i></a>
-                    </div>
-                </div>
-            </div>
-            <div className="row justify-content-center">             
-                <div className="col-auto">
-                    <p className="text-muted">© 2020 Swasthya</p>
-                </div>
-            </div>
+  return (
+    <div id="footer">
+      <div className="container footer-container py-4">
+        <div>
+          <h2 className="mr-auto" href="/">
+            <span className="fa fa-stethoscope"></span> Swasthya
+          </h2>
+          <p>
+            Welcome to Swasthya, our mission here is to connect with the people
+            of India and provide them with best medical information.
+          </p>
         </div>
+
+        <div>
+          <h2>Site Links</h2>
+          <ul className="list list-unstyled">
+            <li>
+              <Link className="nav-link col" to="/home">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link className="nav-link col" to="/aboutus">
+                About us
+              </Link>
+            </li>
+            <li>
+              <Link className="nav-link col" to="/menu">
+                Menu
+              </Link>
+            </li>
+            <li>
+              <Link className="nav-link col" to="/contactus">
+                Contact us
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h2>Address</h2>
+          <p>
+            <i class="fa fa-home"></i> Shri Mata Vaishno Devi University
+          </p>
+          <p>
+            <i class="fa fa-envelope"></i> myswasthya@gmail.com
+          </p>
+          <p>
+            <i class="fa fa-phone"></i> 0120-420-0420
+          </p>
+        </div>
+        <div id="e">
+          <p>@ Copyright 2020 - Swasthya. All rights reserved</p>
+          <div className="text-center">
+            <a href="http://google.com/+">
+              <i className="fa fa-google-plus"></i>
+            </a>
+            <a href="http://www.facebook.com/profile.php?id=">
+              <i className="fa fa-facebook"></i>
+            </a>
+            <a href="http://www.linkedin.com/in/">
+              <i className="fa fa-linkedin"></i>
+            </a>
+            <a href="http://twitter.com/">
+              <i className="fa fa-twitter"></i>
+            </a>
+            <a href="mailto:">
+              <i className="fa fa-envelope-o"></i>
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
-    )
+  );
 }
 
 export default Footer;
