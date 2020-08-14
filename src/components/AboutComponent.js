73c59d139d031888b598945861c8e39de6680b22
Bhanu Pratap Singh <34330311+bhanu4444@users.noreply.github.com>
2020-08-14 23:47:12 +0530

    Changes in About us page

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -7,20 +7,56 @@ function About() {
     <div >
         <img src="/assets/images/HealthQuote.jpg"  className="health-quote"/>
         <div className="container about-container">
-        <h2>About us</h2>
+        <h1>About us</h1>
         <hr  className="hr-style"/>
         
-          <p>
+        <p>
           Welcome to Swasthya, our mission here is to connect with the people of India and 
           provide them with best medical information. Users can check for symptoms for the 
           diseases and find the best suitable medicines and drugs. This platform will also 
           help doctors to publish articles and blogs related to their research field.
          <br/><br/>
-          So join with us on our mission to make India healthy.</p> 
+          So join with us on our mission to make India healthy.
+          </p> 
 
-          
-          <p>Sincerely,<br/> Swasthya team</p>
-          <hr className="hr-style"/>       
+        <p>Sincerely,<br/> Swasthya team</p>
+         
+        <br />
+        <h4 className="h4-style">Our Team</h4>  
+        <hr className="hr-style" />
+        <p>We are a group of technology enthuisasts, developers and designers 
+          who want to help with problems faced by the healthcare system in India.
+          We want to use technology to help others and diminish the gap between people and their health.
+        </p>
+
+        <div className="row">
+          <div className="col-lg-6">
+            <img className="circular" src="/assets/images/aditya.png" />Aditya Raina
+                      </div>
+          <div className="col-lg-6">
+            <img className="circular" src="/assets/images/archit.png" />Archit Dubey
+                      </div>
+          <div className="col-lg-6">
+            <img className="circular" src="/assets/images/ayush.png" /> Ayush Pradhan
+                      </div>
+          <div className="col-lg-6">
+            <img className="circular" src="/assets/images/bhanu.jpg" />Bhanu Pratap Singh
+                      </div>
+          <div className="col-lg-6">
+            <img className="circular" src="/assets/images/puneet.png" />Puneet Sharma
+                      </div>
+        </div>
+
+   
+                    
+                    
+                     
+                
+                
+            
+        <hr className="hr-style" />
+        <p>Here is a Tedex talk about Improving healthcare</p>
+        <iframe  src="https://www.youtube.com/embed/U3MtvvNjUR4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>      
       </div>
     </div>
       
