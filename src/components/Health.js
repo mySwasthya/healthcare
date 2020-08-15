acac6ca07201ca5972ef482edf5291f311537c91
Puneet112 <puneetsoodan112@gmail.com>
2020-08-15 19:20:19 +0530

    Health Component

--- src/components/Health.js
+++ src/components/Health.js
@@ -32,14 +32,204 @@ function Health() {
                 great shape. However, if you focus on nutrition and exercise,
                 you can make a lot of healthy physical changes. Don't forget
                 that a positive attitude is also really important to staying fit
-                and beautifully healthy. If you are looking for the path toward
-                a healthier you? It's not hard to find. The journey begins with
-                some simple tweaks to your lifestyle.{" "}
-                <strong>
-                  The right diet, exercise, and stress-relief plan all play a
-                  big role.
-                </strong>
+                and beautifully healthy.
               </p>
+            </div>
+          </div>
+          <div className="tips">
+            <div className="tips-header">
+              <span>
+                Tips to stay Healthy and Fit
+                <hr />
+              </span>
+            </div>
+            <div>
+              If you are looking for the path toward a healthier you? It's not
+              hard to find. The journey begins with some simple tweaks to your
+              lifestyle. The right diet, exercise, and stress-relief plan all
+              play a big role.
+            </div>
+            <div className="tips-content">
+              <div className="tips-point">
+                <h5>
+                  <strong>Exercising Effectively</strong>
+                </h5>
+                <div>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  Optio laboriosam, laudantium cupiditate rem eveniet at quae
+                  officiis ut accusantium animi nihil! Expedita iure autem ipsam
+                  impedit non culpa obcaecati enim. Lorem ipsum dolor sit amet,
+                  consectetur adipisicing elit. Similique, accusamus cum!
+                  Dolorum iure obcaecati laborum sit ullam illo sed et iste,
+                  eligendi corporis placeat repellendus, aliquam porro ratione
+                  expedita omnis. Lorem ipsum dolor sit, amet consectetur
+                  adipisicing elit. Ipsam eum natus optio quia expedita tempore.
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Libero, natus esse quam eveniet dolorem deleniti repellat
+                  nostrum at? Deserunt ratione culpa modi accusantium magnam
+                  cumque quod eligendi eos nihil neque.
+                </div>
+              </div>
+            </div>
+            <div className="tips-point">
+              <h5>
+                <strong>Cleanse your body</strong>
+              </h5>
+              <div>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio
+                laboriosam, laudantium cupiditate rem eveniet at quae officiis
+                ut accusantium animi nihil! Expedita iure autem ipsam impedit
+                non culpa obcaecati enim. Lorem ipsum dolor sit amet,
+                consectetur adipisicing elit. Similique, accusamus cum! Dolorum
+                iure obcaecati laborum sit ullam illo sed et iste, eligendi
+                corporis placeat repellendus, aliquam porro ratione expedita
+                omnis. Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Ipsam eum natus optio quia expedita tempore. Lorem ipsum dolor
+                sit amet consectetur adipisicing elit. Libero, natus esse quam
+                eveniet dolorem deleniti repellat nostrum at? Deserunt ratione
+                culpa modi accusantium magnam cumque quod eligendi eos nihil
+                neque.
+              </div>
+            </div>
+            <div className="tips-point">
+              <div className="row ml-0">
+                <div className="col-12 col-md-7 order-2 order-md-1 pl-0 ml-0">
+                  <h5>
+                    <strong>Maintain healthy diet</strong>
+                  </h5>
+                  <div>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Optio laboriosam, laudantium cupiditate rem eveniet at quae
+                    officiis ut accusantium animi nihil! Expedita iure autem
+                    ipsam impedit non culpa obcaecati enim. Lorem ipsum dolor
+                    sit amet, consectetur adipisicing elit. Similique, accusamus
+                    cum! Dolorum iure obcaecati laborum sit ullam illo sed et
+                    iste, eligendi corporis placeat repellendus, aliquam porro
+                    ratione expedita omnis. Lorem ipsum dolor sit, amet
+                    consectetur adipisicing elit. Ipsam eum natus optio quia
+                    expedita tempore. Lorem ipsum dolor sit amet consectetur
+                    adipisicing elit. Libero, natus esse quam eveniet dolorem
+                    deleniti repellat nostrum at? Deserunt ratione culpa modi
+                    accusantium magnam cumque quod eligendi eos nihil neque.
+                  </div>
+                </div>
+                <div className="col-12 col-md-5 order-1 order-md-2 pr-0 ml-0 mb-4">
+                  <img
+                    src="/assets/images/diet.jpg"
+                    className="img-fluid img-height"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="tips-point">
+              <h5>
+                <strong>Having a Healthy Mindset</strong>
+              </h5>
+              <div>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio
+                laboriosam, laudantium cupiditate rem eveniet at quae officiis
+                ut accusantium animi nihil! Expedita iure autem ipsam impedit
+                non culpa obcaecati enim. Lorem ipsum dolor sit amet,
+                consectetur adipisicing elit. Similique, accusamus cum! Dolorum
+                iure obcaecati laborum sit ullam illo sed et iste, eligendi
+                corporis placeat repellendus, aliquam porro ratione expedita
+                omnis. Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Ipsam eum natus optio quia expedita tempore. Lorem ipsum dolor
+                sit amet consectetur adipisicing elit. Libero, natus esse quam
+                eveniet dolorem deleniti repellat nostrum at? Deserunt ratione
+                culpa modi accusantium magnam cumque quod eligendi eos nihil
+                neque.
+              </div>
+            </div>
+            <div className="tips-point">
+              <h5>
+                <strong>Follow healthy sleeping patterns</strong>
+              </h5>
+              <div>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio
+                laboriosam, laudantium cupiditate rem eveniet at quae officiis
+                ut accusantium animi nihil! Expedita iure autem ipsam impedit
+                non culpa obcaecati enim. Lorem ipsum dolor sit amet,
+                consectetur adipisicing elit. Similique, accusamus cum! Dolorum
+                iure obcaecati laborum sit ullam illo sed et iste, eligendi
+                corporis placeat repellendus, aliquam porro ratione expedita
+                omnis. Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Ipsam eum natus optio quia expedita tempore. Lorem ipsum dolor
+                sit amet consectetur adipisicing elit. Libero, natus esse quam
+                eveniet dolorem deleniti repellat nostrum at? Deserunt ratione
+                culpa modi accusantium magnam cumque quod eligendi eos nihil
+                neque.
+              </div>
+            </div>
+            <div className="tips-point">
+              <div className="row ml-0">
+                <div className="col-12 col-md-7 order-2 order-md-1 pl-0 ml-0">
+                  <h5>
+                    <strong>Yoga and Meditation</strong>
+                  </h5>
+                  <div>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Optio laboriosam, laudantium cupiditate rem eveniet at quae
+                    officiis ut accusantium animi nihil! Expedita iure autem
+                    ipsam impedit non culpa obcaecati enim. Lorem ipsum dolor
+                    sit amet, consectetur adipisicing elit. Similique, accusamus
+                    cum! Dolorum iure obcaecati laborum sit ullam illo sed et
+                    iste, eligendi corporis placeat repellendus, aliquam porro
+                    ratione expedita omnis. Lorem ipsum dolor sit, amet
+                    consectetur adipisicing elit. Ipsam eum natus optio quia
+                    expedita tempore. Lorem ipsum dolor sit amet consectetur
+                    adipisicing elit. Libero, natus esse quam eveniet dolorem
+                    deleniti repellat nostrum at? Deserunt ratione culpa modi
+                    accusantium magnam cumque quod eligendi eos nihil neque.
+                  </div>
+                </div>
+                <div className="col-12 col-md-5 order-1 order-md-2 pr-0 ml-0 mb-4">
+                  <img
+                    src="/assets/images/meditation.jpg"
+                    className="img-fluid img-height"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="tips-point">
+              <h5>
+                <strong>Quit smoking and drinking</strong>
+              </h5>
+              <div>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio
+                laboriosam, laudantium cupiditate rem eveniet at quae officiis
+                ut accusantium animi nihil! Expedita iure autem ipsam impedit
+                non culpa obcaecati enim. Lorem ipsum dolor sit amet,
+                consectetur adipisicing elit. Similique, accusamus cum! Dolorum
+                iure obcaecati laborum sit ullam illo sed et iste, eligendi
+                corporis placeat repellendus, aliquam porro ratione expedita
+                omnis. Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Ipsam eum natus optio quia expedita tempore. Lorem ipsum dolor
+                sit amet consectetur adipisicing elit. Libero, natus esse quam
+                eveniet dolorem deleniti repellat nostrum at? Deserunt ratione
+                culpa modi accusantium magnam cumque quod eligendi eos nihil
+                neque.
+              </div>
+            </div>
+            <div className="tips-point">
+              <h5>
+                <strong>Visit Your Doctor</strong>
+              </h5>
+              <div>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio
+                laboriosam, laudantium cupiditate rem eveniet at quae officiis
+                ut accusantium animi nihil! Expedita iure autem ipsam impedit
+                non culpa obcaecati enim. Lorem ipsum dolor sit amet,
+                consectetur adipisicing elit. Similique, accusamus cum! Dolorum
+                iure obcaecati laborum sit ullam illo sed et iste, eligendi
+                corporis placeat repellendus, aliquam porro ratione expedita
+                omnis. Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Ipsam eum natus optio quia expedita tempore. Lorem ipsum dolor
+                sit amet consectetur adipisicing elit. Libero, natus esse quam
+                eveniet dolorem deleniti repellat nostrum at? Deserunt ratione
+                culpa modi accusantium magnam cumque quod eligendi eos nihil
+                neque.
+              </div>
             </div>
           </div>
         </div>
