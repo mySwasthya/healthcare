843fa5ae1b39251297b2631da6d8eb1ae4cd67f8
Ayush <ayupradhan1999@gmail.com>
2020-08-18 13:29:10 +0530

    updated services

--- src/components/Services.js
+++ src/components/Services.js
@@ -17,8 +17,8 @@ function Services() {
           <div className="col-sm-12 col-md-12 col-lg-6 col-xl-6">
             <div className="tm-flex-center p-5">
               <q className="tm-quote tm-text-color-gray">
-                Ut sit amet augue elit. Vivamus eget tortor in ante scelerisque
-                gravida. Vestibulum auctor condimentum sem.
+                Maintain your personal health and medicinal records efficiently
+                and securely
               </q>
             </div>
             {/* <Button className="btn">
@@ -34,7 +34,7 @@ function Services() {
             <div className="tm-flex-center p-5">
               <div className="tm-md-flex-center">
                 <h2 className="tm-text-color-primary mb-4">
-                  Fusce ac enim at justo
+                  Online consultation
                 </h2>
                 <p className="mb-4">
                   Pellentesque sagittis feugiat massa, vitae blandit elit dictum
@@ -44,6 +44,7 @@ function Services() {
                 {/* <Button className="btn">
                   <a href="/">Read More</a>
                 </Button> */}
+                <Button className="mr-3">Book Appointment</Button>
               </div>
             </div>
           </div>
@@ -63,10 +64,15 @@ function Services() {
           </div>
           <div className="col-sm-12 col-md-12 col-lg-6 col-xl-6">
             <div className="tm-flex-center p-5">
-              <q className="tm-quote tm-text-color-gray">
-                Ut sit amet augue elit. Vivamus eget tortor in ante scelerisque
-                gravida. Vestibulum auctor condimentum sem.
-              </q>
+              {/* <h2 className="tm-text-color-primary mb-4">
+                Online consultation
+              </h2> */}
+              <p className="tm-quote tm-text-color-gray">
+                Keep youself updated of the latest health discoveries to how to
+                prevent some diseaser with
+                <a href="\home"> blogs and articles</a> from Physicians and
+                Specialists
+              </p>
             </div>
             {/* <Button className="btn">
               <a href="/">Read More</a>
@@ -78,16 +84,16 @@ function Services() {
             <div className="tm-flex-center p-5">
               <div className="tm-md-flex-center">
                 <h2 className="tm-text-color-primary mb-4">
-                  Fusce ac enim at justo
+                  E-pharmacy and Health Supplements
                 </h2>
                 <p className="mb-4">
-                  Pellentesque sagittis feugiat massa, vitae blandit elit dictum
-                  in. Nam eleifend nunc dui, sed cusus justo molestie id.
-                  Vestibulum vel sagittis justo.
+                  Order medicines and Health supplements from your local
+                  authorised pharmacy and get it in 1 hour.
                 </p>
                 {/* <Button className="btn">
                   <a href="/">Read More</a>
                 </Button> */}
+                <Button className="mr-3">E-pharmacy</Button>
               </div>
             </div>
           </div>
@@ -108,8 +114,8 @@ function Services() {
           <div className="col-sm-12 col-md-12 col-lg-6 col-xl-6">
             <div className="tm-flex-center p-5">
               <q className="tm-quote tm-text-color-gray">
-                Ut sit amet augue elit. Vivamus eget tortor in ante scelerisque
-                gravida. Vestibulum auctor condimentum sem.
+                We have contacted with local cabs for pick & drop from Clinics
+                and Hospitals......
               </q>
             </div>
             {/* <Button className="btn">
@@ -121,17 +127,14 @@ function Services() {
           <div className="col-sm-12 col-md-12 col-lg-6 col-xl-6">
             <div className="tm-flex-center p-5">
               <div className="tm-md-flex-center">
-                <h2 className="tm-text-color-primary mb-4">
-                  Fusce ac enim at justo
-                </h2>
+                <h2 className="tm-text-color-primary mb-4">Symptom checker</h2>
                 <p className="mb-4">
-                  Pellentesque sagittis feugiat massa, vitae blandit elit dictum
-                  in. Nam eleifend nunc dui, sed cusus justo molestie id.
-                  Vestibulum vel sagittis justo.
+                  Check if you have symptoms of any disease !
                 </p>
                 {/* <Button className="btn">
                   <a href="/">Read More</a>
                 </Button> */}
+                <Button className="mr-3">Let's check</Button>
               </div>
             </div>
           </div>
@@ -172,9 +175,9 @@ function Services() {
                   aliquet ac pharetra in, suscipit eu risus. Etiam rutrum eros
                   ultrices, consectetur felis ultrices, vehicula purus.
                 </p>
-                <Button size="lg" className="button1">
+                {/* <Button size="lg" className="button1">
                   <Link to="/appointment">Book Appointment</Link>
-                </Button>
+                </Button> */}
               </div>
             </div>
           </div>
